3fff55103218cec81968cee649c221829a23e073
k-nasa <htilcs1115@gmail.com>
2019-12-08 23:34:59 +0900

    Add base

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
     println!("Hello, world!");
 }
+
+fn tyozo<S: Into<String>>(input: S) -> Result<(), ()> {
+    // TODO parse command
+
+    // TODO exec command
+
+    // TODO return exec result
+
+    Ok(())
+}
+
+#[test]
+fn test_tyozo() {
+    assert_eq!(tyozo(""), Ok(()))
+}
