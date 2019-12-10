5f47bc273f044686e65971acff9028590cf5d01e
k-nasa <htilcs1115@gmail.com>
2019-12-10 21:55:19 +0900

    Add methods to Lexer struct

--- src/parser.rs
+++ src/parser.rs
@@ -22,6 +22,55 @@ impl Lexer {
             current_ch: '\0',
         }
     }
+
+    pub fn current_ch(&self) -> char {
+        self.current_ch
+    }
+
+    fn get_char(&self, position: usize) -> char {
+        match self.input.chars().nth(position) {
+            Some(c) => c,
+            None => '\0',
+        }
+    }
+
+    pub fn read_char(&mut self) {
+        self.current_ch = self.get_char(self.current_position);
+        self.current_position = self.read_position;
+        self.read_position += 1;
+    }
+
+    pub fn read_string_literal(&mut self) -> Result<String, String> {
+        if self.current_ch != '"' {
+            return Err("failed to read string literal".to_string());
+        }
+
+        self.read_char();
+
+        let mut s = String::new();
+
+        while self.current_ch != '"' {
+            s.push(self.current_ch);
+            self.read_char();
+        }
+
+        Ok(s)
+    }
+
+    pub fn read_identifier(&mut self) -> String {
+        let start_position = self.current_position - 1;
+
+        while self.current_ch != ' ' && !self.is_end() {
+            self.read_char();
+        }
+
+        let end_position = self.current_position - 1;
+        self.input[start_position..end_position].to_string()
+    }
+
+    pub fn is_end(&self) -> bool {
+        self.current_ch() == '\n' || self.current_ch() == '\0'
+    }
 }
 
 fn split_input<S: Into<String>>(input: S) -> Result<SplitedCommand, String> {
