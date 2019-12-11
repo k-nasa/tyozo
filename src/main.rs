6ded01695c8c06368994fe4fa2fce17bd22c9538
k-nasa <htilcs1115@gmail.com>
2019-12-11 11:27:32 +0900

    remove code

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,10 @@ mod lexer;
 mod parser;
 
 fn main() -> Result<(), String> {
-    println!("Hello, world!");
     tyozo("hoge")
 }
 
 fn tyozo<S: Into<String>>(input: S) -> Result<(), String> {
-    // TODO parse command
     parser::parse(input)?;
 
     // TODO exec command
