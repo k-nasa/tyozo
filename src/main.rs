9632a0cef3ce5551c1d85d04f42056241e48a7c4
k-nasa <htilcs1115@gmail.com>
2019-12-13 08:41:59 +0900

    Add cli prototype

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,30 @@
+use ansi_term::Colour::Red;
+use std::io::Write;
 use tyozo::Memdb;
 
 fn main() -> Result<(), String> {
     let mut db = Memdb::new();
-    db.exec("hoge")?;
 
+    loop {
+        print!(">> ");
+        std::io::stdout().flush().unwrap();
+
+        let input: String = read();
+
+        if &input == "exit" {
+            break;
+        }
+
+        match db.exec(input) {
+            Err(e) => println!("(error) {}", Red.bold().paint(e)),
+            Ok(s) => println!("{}", s),
+        }
+    }
     Ok(())
+}
+
+fn read<T: std::str::FromStr>() -> T {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().parse().ok().unwrap()
 }
