c9c14dda8280e3585ee0c4a1ea272a5c02daee6e
k-nasa <htilcs1115@gmail.com>
2019-12-13 08:17:07 +0900

    test: Move test to doc test

--- src/lib.rs
+++ src/lib.rs
@@ -25,6 +25,27 @@ impl Memdb {
         }
     }
 
+    /// # Example
+    /// ```
+    /// use tyozo::Memdb;
+    /// let mut db = Memdb::new();
+    ///
+    /// let result = db.exec("set hoge value");
+    /// assert_eq!(result, Ok(String::from("OK")));
+    ///
+    /// let result = db.exec("get hoge");
+    /// assert_eq!(result, Ok(String::from("value")));
+    ///
+    /// db.exec("set fuga value").unwrap();
+    /// let result = db.exec("del hoge fuga");
+    /// assert_eq!(result, Ok(String::from("2")));
+    ///
+    /// let result = db.exec("setnx hoge value");
+    /// assert!(result.is_ok());
+    ///
+    /// let result = db.exec("setnx hoge value");
+    /// assert!(result.is_err());
+    /// ```
     pub fn exec<S: Into<String>>(&mut self, input: S) -> Result<String, String> {
         let command = parser::parse(input)?;
 
@@ -52,11 +73,37 @@ impl Memdb {
         }
     }
 
+    /// # Example
+    /// ```
+    /// use tyozo::Memdb;
+    /// let mut memdb = Memdb::new();
+    ///
+    /// memdb.set("key", "value");
+    /// assert_eq!(memdb.inner().get("key"), Some(&b"value".to_vec()));
+    ///
+    /// // value is override
+    /// memdb.set("key", "next value");
+    /// assert_eq!(memdb.inner().get("key"), Some(&b"next value".to_vec()));
+    /// ```
     pub fn set(&mut self, key: impl AsRef<str>, value: impl AsRef<[u8]>) {
         self.inner
             .insert(key.as_ref().to_owned(), value.as_ref().to_owned());
     }
 
+    /// # Example
+    /// ```
+    /// use tyozo::Memdb;
+    /// let mut memdb = Memdb::new();
+
+    /// let result = memdb.setnx("key", "value");
+    /// assert!(result.is_ok());
+    /// assert_eq!(memdb.inner().get("key"), Some(&b"value".to_vec()));
+
+    /// // value is not override
+    /// let result = memdb.setnx("key", "next value");
+    /// assert!(result.is_err());
+    /// assert_eq!(memdb.inner().get("key"), Some(&b"value".to_vec()));
+    /// ```
     pub fn setnx(&mut self, key: impl AsRef<str>, value: impl AsRef<[u8]>) -> Result<(), String> {
         if self.get(&key).is_some() {
             return Err(String::from("ERR key is already exists"));
@@ -68,10 +115,35 @@ impl Memdb {
         Ok(())
     }
 
+    /// # Exmaple
+    /// ```
+    /// use tyozo::Memdb;
+    /// let mut memdb = Memdb::new();
+    ///
+    /// memdb.set("key", "value");
+    ///
+    /// assert_eq!(memdb.get("key"), Some(b"value".to_vec()));
+    ///
+    /// assert_eq!(memdb.get("not setted key"), None);
+    /// ```
     pub fn get(&mut self, key: impl AsRef<str>) -> Option<Vec<u8>> {
         self.inner.get(&key.as_ref().to_owned()).cloned()
     }
 
+    /// # Exmaple
+    /// ```
+    /// use tyozo::Memdb;
+    /// let mut memdb = Memdb::new();
+    /// memdb.set("key", "value");
+    ///
+    /// let delete_count = memdb.del(vec!["key", "key2"]);
+    ///
+    /// assert_eq!(delete_count, 1);
+    /// assert_eq!(memdb.get("key"), None);
+    ///
+    /// let delete_count = memdb.del(vec!["key"]);
+    /// assert_eq!(delete_count, 0);
+    /// ```
     pub fn del(&mut self, keys: Vec<impl AsRef<str>>) -> usize {
         keys.into_iter()
             .map(|key| self.inner.remove(&key.as_ref().to_owned()))
@@ -81,61 +153,5 @@ impl Memdb {
 
     pub fn inner(&self) -> &MemdbInner {
         &self.inner
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_memdb_set() {
-        let mut memdb = Memdb::new();
-
-        memdb.set("key", "value");
-        assert_eq!(memdb.inner().get("key"), Some(&b"value".to_vec()));
-
-        // value is override
-        memdb.set("key", "next value");
-        assert_eq!(memdb.inner().get("key"), Some(&b"next value".to_vec()));
-    }
-
-    #[test]
-    fn test_memdb_setnx() {
-        let mut memdb = Memdb::new();
-
-        let result = memdb.setnx("key", "value");
-        assert!(result.is_ok());
-        assert_eq!(memdb.inner().get("key"), Some(&b"value".to_vec()));
-
-        // value is not override
-        let result = memdb.setnx("key", "next value");
-        assert!(result.is_err());
-        assert_eq!(memdb.inner().get("key"), Some(&b"value".to_vec()));
-    }
-
-    #[test]
-    fn test_memdb_get() {
-        let mut memdb = Memdb::new();
-
-        memdb.set("key", "value");
-
-        assert_eq!(memdb.get("key"), Some(b"value".to_vec()));
-
-        assert_eq!(memdb.get("not setted key"), None);
-    }
-
-    #[test]
-    fn test_memdb_delete() {
-        let mut memdb = Memdb::new();
-        memdb.set("key", "value");
-
-        let delete_count = memdb.del(vec!["key", "key2"]);
-
-        assert_eq!(delete_count, 1);
-        assert_eq!(memdb.get("key"), None);
-
-        let delete_count = memdb.del(vec!["key"]);
-        assert_eq!(delete_count, 0);
     }
 }
