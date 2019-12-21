2704947b729c9ea6115c641642dbf8f2914da7d0
k-nasa <htilcs1115@gmail.com>
2019-12-21 12:07:16 +0900

    Add logging

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,38 @@ use std::io::Write;
 use tyozo::Memdb;
 
 const DB_FILE_PATH: &str = "./tyozo.db";
+const LOG_FILE_PATH: &str = "./tyozo.log";
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut f = std::fs::OpenOptions::new()
+    let mut db_file = std::fs::OpenOptions::new()
         .append(true)
         .read(true)
         .create(true)
         .open(DB_FILE_PATH)?;
 
     let mut contents = String::new();
-    f.read_to_string(&mut contents)?;
+    db_file.read_to_string(&mut contents)?;
 
     let mut db = Memdb::deserialize(contents.as_bytes())?;
+
+    let mut log_file = std::fs::OpenOptions::new()
+        .append(true)
+        .read(true)
+        .create(true)
+        .open(LOG_FILE_PATH)?;
+
+    let mut logs = String::new();
+    log_file.read_to_string(&mut logs)?;
+
+    logs.lines().for_each(|log| {
+        let _ = db.exec(log);
+    });
+
+    // initialize log file
+    std::fs::OpenOptions::new()
+        .write(true)
+        .truncate(true)
+        .open(LOG_FILE_PATH)?;
 
     loop {
         print!(">> ");
@@ -28,16 +48,29 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         if &input == "exit" {
             let serialized = db.serialize();
 
-            f.write_all(&serialized)?;
-            f.flush()?;
+            db_file.write_all(&serialized)?;
+            db_file.flush()?;
+
+            // initialize log file
+            std::fs::OpenOptions::new()
+                .write(true)
+                .truncate(true)
+                .open(LOG_FILE_PATH)?;
+
             break;
         }
+
+        // logging
+        log_file.write_all(&input.as_bytes())?;
+        log_file.write_all(&[b'\n'])?;
+        log_file.flush()?;
 
         match db.exec(input) {
             Err(e) => println!("(error) {}", Red.bold().paint(e)),
             Ok(s) => println!("{}", s),
         }
     }
+
     Ok(())
 }
 
